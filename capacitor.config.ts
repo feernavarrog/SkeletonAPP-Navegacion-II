041feb6d80c2197958941915005b8588b31fc6c9
Fernando <fer.navarrog@duocuc.cl>
2024-06-09 15:59:38 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SkeletonAPP-Navegacion-II',
+  webDir: 'www'
+};
+
+export default config;
